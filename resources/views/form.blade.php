ea9af81991c7144d0657d89dbd2a253a95f446c9
reddowell <vikt.anmail@gmail.com>
2018-11-18 17:31:21 +0300

    fix

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -5,8 +5,7 @@
 @section('content')
     <div class="jumbotron">
         <h1 class="display-4">Page Analyzer</h1>
-        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to
-            featured content or information.</p>
+        <p class="lead">Simple page that reflects the core values of the page</p>
         <hr class="my-4">
         <p>Enter URL for testing:</p>
         <div class="mb-3">
